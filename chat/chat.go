a811733aaf642b7ce1f732eb32e3e5ed4a790883
최치원 <cw.choiit@choechiwon-ui-MacBookPro.local>
2021-09-18 10:58:54 +0900

    #02 Chat Server Connection

--- chat/chat.go
+++ chat/chat.go
@@ -3,11 +3,16 @@ package chat
 import (
 	"net/http"
 
+	"github.com/chiwon99881/gone-chat/utils"
 	"github.com/gorilla/websocket"
 )
 
 var upgrader = websocket.Upgrader{}
 
 func Upgrade(rw http.ResponseWriter, r *http.Request) {
-	upgrader.Upgrade(rw, r, nil)
+	upgrader.CheckOrigin = func(r *http.Request) bool {
+		return true
+	}
+	_, err := upgrader.Upgrade(rw, r, nil)
+	utils.HandleError(err)
 }
